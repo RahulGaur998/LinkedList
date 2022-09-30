68354470ed1fc46cd77651c6db0944848ad461f7
RahulGaur998 <gaur.rahul996@gmail.com>
2022-09-30 22:25:44 +0530

    ADD: Added function to sort linked list using bubble sort

--- src/main/java/com/linkedlistpractise/App.java
+++ src/main/java/com/linkedlistpractise/App.java
@@ -22,5 +22,8 @@ public class App {
         list.showLinkedList();
         list.searchNodeWithKey(30);
         list.sizeOfLinkedList();
+        list.orderedLinkedList();
+        list.showLinkedList();
+
     }
 }
